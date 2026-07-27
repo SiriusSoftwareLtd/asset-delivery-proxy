6aa64c6d81f239d59a8faefd2002c3955934fa72
Max Thakur <dev@maxthakur.xyz>
2026-07-27 12:15:02 -0400

    refactor(worker): :white_check_mark: Update tests to use the new dependencies

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,29 +1,10 @@
-import {
-	env,
-	createExecutionContext,
-	waitOnExecutionContext,
-	SELF,
-} from "cloudflare:test";
-import { describe, it, expect } from "vitest";
-import worker from "../src/index";
+import { env } from 'cloudflare:test'
+import { describe, expect, test } from 'vitest'
+import worker from '../src'
 
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
-
-describe("Hello World worker", () => {
-	it("responds with Hello World! (unit style)", async () => {
-		const request = new IncomingRequest("http://example.com");
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
-
-	it("responds with Hello World! (integration style)", async () => {
-		const response = await SELF.fetch("https://example.com");
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
-});
+describe('Test the application', () => {
+	test('Should return 200 response', async () => {
+		const res = await worker.request('http://localhost/')
+		expect(res.status).toBe(200)
+	})
+})
