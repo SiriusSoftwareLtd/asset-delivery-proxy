0e0c83c06cad0e0544705b6982451a2e359aa074
Max T <25856189+MaxTCodes@users.noreply.github.com>
2026-08-05 13:34:18 -0400

    test(extension): 🧪 cover Roblox XML whitespace

--- test/assetExtension.spec.ts
+++ test/assetExtension.spec.ts
@@ -57,7 +57,7 @@ describe('asset extension detection', () => {
   });
 
   test('uses Roblox serialization and asset type to distinguish model/place formats', () => {
-    expect(extensionFromPrefix(new TextEncoder().encode('  \ufeff<roblox'), 9)).toBe('.rbxlx');
+    expect(extensionFromPrefix(new TextEncoder().encode('  \ufeff \t\r\n<roblox'), 9)).toBe('.rbxlx');
     expect(extensionFromPrefix(new TextEncoder().encode('<roblox!'), 8)).toBe('.rbxm');
     expect(extensionFromPrefix(new TextEncoder().encode('version 1.00'))).toBe('.mesh');
   });
